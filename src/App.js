772e4975953b238ba533c1b767bd60a9e0d43015
kangdonguk <kangdongouk@gmail.com>
2021-04-13 09:45:22 +0900

    #2.2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React from "react";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({ name, image }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={image} alt="" />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samguiosak" />
-      <Food fav="chukumi" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} image={dish.image} />
+      ))}
     </div>
   );
 }
