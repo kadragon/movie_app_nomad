ece60b3d097c40f99957d27e01bcd206d4a76eb9
kangdonguk <kangdongouk@gmail.com>
2021-04-13 12:55:02 +0900

    #2.4

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
     name: "kimchi",
     image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
+    rating: 5.0,
   },
   {
     id: 2,
     name: "Samgyeopsal",
     image:
       "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+    rating: 4.2,
   },
 ];
 
-function Food({ name, image }) {
+function Food({ name, image, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
       <img src={image} alt={name} />
     </div>
   );
 }
+
+// propType 확인
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} image={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          image={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
